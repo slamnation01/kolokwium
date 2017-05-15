500cab233d10930fcda750101c7fb30e3c8364a5
kamil <slamnation01@gmail.com>
2017-05-15 14:43:09 +0200

    Initial

---
+++ Kol_slam/Kol_slam/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kol_slam {
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
---
+++ Kol_slam/Kol_slamTests/RectangleTests.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kol_slamTests {
+    [TestClass]
+    public class RectangleTests {
+        [TestMethod]
+        public void TestMethod1() {
+        }
+    }
+}
